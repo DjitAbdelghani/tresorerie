cf7c0172b7424a3c0eef2ba794654424997e319a
abdelghani taleb <abdelghani.taleb.djit@gmail.com>
2022-10-25 08:45:59 +0000

    tresorerie

---
+++ tresorerie/tresorerie/doctype/solde_initial/test_solde_initial.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, nassim and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSoldeinitial(FrappeTestCase):
+	pass
